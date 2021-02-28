6e6b78d9c6000d8aa934d41f4e447df8abe0ae7b
Débora <deboracristinapereira123@gmail.com>
2021-02-27 22:51:34 -0300

    Aulas JS

--- js/script.js
+++ js/script.js
@@ -72,6 +72,26 @@
         - parâmetros: quando a função é criada/declarada
 
     - quando não tem return, retorna no console como undefined
+    - se a função é chamada antes da sua declaração, o JS interpreta como se fosse declarada e chamada depois, executando normalmente (elevação)
+
+        - funciona se não for função anônima, se a função não for atribuida a uma variável. Se for feito isso, a variável é undefined e ele tenta executar uma função com esse valor, dando erro
+    
+    - arrow function: 
+
+        - sem a palavra chave function
+        - geralmente a função é atribuida a uma const
+
+    - callback function:
+
+        - o parâmetro de uma função é outra função, podendo ser arrow ou uma função normal
+
+    - função construtora:
+
+        - cria um novo objeto, similar ao construtor no Java
+        - expressão new
+        - molde do objeto (atributos e função em uma classe - java)
+        - pode ser feito como json string ou json object
+        - funções padrões JS: Date, String, Number, etc.
 
 */
 
@@ -199,7 +219,7 @@ console.log(myName, age);
 
 console.log(`Nome: ${myName}, Idade: ${age} anos.`);
 
-// Funções sendo declaradas dentro de variáveis
+// Funções sendo declaradas dentro de variáveis (função anônima)
 
 // Sendo var, let ou const, uma variável criada dentro da função é de escopo local.
 
@@ -217,3 +237,45 @@ const result = sum(3, 4);
 
 console.log(`A soma de ${number1} e ${number2} é ${result}`);
 // console.log(total);
+
+// Chamar antes de declarar
+
+console.log(multiply(2, 5));
+
+function multiply(a, b) {
+    return a * b;
+}
+
+// Arrow function
+
+const data = (name, age) => {
+    return `Meu nome é ${name} e tenho ${age} anos.`;
+}
+
+console.log(data("Luiz", 21));
+
+// Callback
+
+function mathExpression(sum) {
+    return (sum + 5)/10;
+}
+
+console.log(mathExpression(sum(7, 8)));
+
+// Função construtora
+
+function Company(name, area, city) {
+    this.name = name;
+    this.area = area;
+    this.city = city;
+}
+
+const someCompany = new Company("Example", "Services", "Joinville");
+
+console.log(someCompany.name);
+console.log(someCompany.area);
+
+// Funções construtoras padrões JS
+
+const date = new Date();
+console.log(date);
