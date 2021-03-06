7393def98c128a383069c25818710b5b9cbaba22
Débora Gonçalves <deboracristinapereira123@gmail.com>
2021-03-06 15:41:15 -0300

    Aulas JS

--- js/script.js
+++ js/script.js
@@ -101,6 +101,17 @@
     - Array com construtor: new Array, passando os itens ou o tamanho do Array
     - Array.from: cada caractere da string será um elemento do array
 
+
+11. Expressões e operadores:
+
+    - Ponto e vírgula é opcional ao final da expressão.
+    - Função autoexecutável
+    - Operador binário: dois valores;
+    - Operador unário: um único valor. Exemplo: typeof, delete
+    - Operador ternário: três valores. if ternário
+    - Expressão new: criar objeto
+    - Operadores de atribuição: módulo e expoencial
+
 */
 
 // Template literals ($) 
@@ -392,3 +403,53 @@ console.log(justSomeArray);
 // Encontrar posição do elemento
 
 console.log(justSomeArray.indexOf("José"));
+
+// Função autoexecutável: não precisa ser chamada
+
+(function() {
+    console.log("Função autoexecutável");
+})()
+
+// Operador binário
+
+let numberOne = 1;
+let numberTwo = 5;
+console.log(numberOne + numberTwo);
+
+// Operador unário: decremento antes e depois
+
+console.log(numberTwo--);
+console.log(--numberTwo);
+
+// Unário - delete
+
+// Propriedade age 
+
+delete trainingObject.age;
+console.log(trainingObject);
+
+// Operador ternário
+
+let expression = (numberOne + numberTwo) / 2;
+
+let evenOrOdd = expression % 2 == 0 ? "Número par" : "Número ímpar";
+console.log(evenOrOdd);
+
+// New - novo objeto
+
+let lastYear = new Date().getFullYear() - 1;
+console.log(lastYear);
+
+// Expoencial sem função pow
+
+console.log(2 ** 3);
+
+// Operadores de atribuição
+
+    // Módulo
+
+    console.log(lastYear %= 2);
+
+    // Expoencial
+
+    console.log(numberTwo **= 2);
